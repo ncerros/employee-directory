2340b0432f6fe01284d7213c6a0ccced1e210fdf
Norman Cerros <n_cerros@hotmail.com>
2021-06-21 14:58:10 -0600

    modified Employees.js

---
+++ src/components/Employees.js
@@ -0,0 +1,34 @@
+import React form "react";
+
+const Employees = (props) => {
+    function formatDate(date) {
+        const dateArray = date.split("-");
+        const year = dateArray[0];
+        const month = dateArray[1];
+        const dayArray = dateArray[2].split("T");
+        const day = dayArray[0];
+        const formattedDate = [month, day, year].join("-");
+    }
+
+let dateOfBirth = formatDate(props.dob);
+return (
+    <tr className="tr">
+    <td>
+        <img alt={props.firstName} src={props.icon} />
+      </td>
+      <td>
+        {props.firstName} {props.lastName}
+      </td>
+      <td>{props.email}</td>
+      <td>{props.phone} </td>
+      <td>{dateOfBirth}</td>
+      <td>{props.address}</td>
+    </tr>
+  );
+};
+
+export default Employees;
+
+          
+
+
